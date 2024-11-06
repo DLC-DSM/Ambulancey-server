e0d72436097129fff6191a16c94e165755af600d
kimsee8200 <ttungseui11@gmail.com>
2024-11-06 22:23:08 +0900

    setting, feature 질병 관리와 도커 설정

--- API/src/main/java/org/example/ApiApplication.java
+++ API/src/main/java/org/example/ApiApplication.java
@@ -3,8 +3,10 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@EntityScan("org.example")
+@EntityScan("org.example")
+@EnableJpaRepositories
 @SpringBootApplication
 public class ApiApplication {
     public static void main(String[] args) {
