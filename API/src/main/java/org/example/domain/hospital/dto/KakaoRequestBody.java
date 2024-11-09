b89ab3b32db1706559adcb079410c7291cb44456
ajswl <kkhhjj1209@gmail.com>
2024-11-09 19:22:54 +0900

    api 생성

---
+++ API/src/main/java/org/example/domain/hospital/dto/KakaoRequestBody.java
@@ -0,0 +1,10 @@
+package org.example.domain.hospital.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class KakaoRequestBody {
+    String query;
+}
